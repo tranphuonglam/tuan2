ef1d65867534832e9ea5603a82a7155d96121a11
tranphuonglam <78217868+tranphuonglam@users.noreply.github.com>
2021-02-26 08:50:50 +0700

    Add files via upload

---
+++ bai17btThemtuan2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	double mon1,mon2,mon3,mon4,mon5;
+	cin >> mon1 >> mon2 >> mon3 >> mon4 >> mon5;
+	cout << (mon1+mon2+mon3+mon4+mon5)/5;
+	return 0;
+}
